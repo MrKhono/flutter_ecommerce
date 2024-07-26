0caebcfdaa74d28add8e7abacd3edef9e9770740
Mrkhono <akhono@yahoo.com>
2024-07-26 21:15:29 +0000

    updated all constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:ecommerce_app/utils/constants/colors.dart';
 import 'package:ecommerce_app/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
+
+import 'app.dart';
 
 void main() {
 
@@ -9,18 +12,8 @@ void main() {
   //Todo: Init Firebase
   //Todo: Initialiaze Authentification
 
+
   runApp(const App());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return  MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-    );
-  }
-}
